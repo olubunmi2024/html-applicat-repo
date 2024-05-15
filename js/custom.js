9ddb06d4188637a67b0aeb3f3c11cc9203160947
olubunmi2024 <bunmioladapo@gmail.com>
2024-05-15 03:19:25 +0000

    initial codes for the project

--- js/custom.js
+++ js/custom.js
@@ -1,31 +1,31 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
 
-getYear();
+  (function ($) {
+  
+  "use strict";
 
-// overlay menu
-function openNav() {
-    document.getElementById("myNav").classList.toggle("menu_width");
-    document.querySelector(".custom_menu-btn").classList.toggle("menu_btn-style");
-}
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
+    });
 
+    // NAVBAR
+    $(".navbar").headroom();
 
-/** google_map js **/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
 
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+    $('.slick-slideshow').slick({
+      autoplay: true,
+      infinite: true,
+      arrows: false,
+      fade: true,
+      dots: true,
+    });
 
-// lightbox gallery
-$(document).on("click", '[data-toggle="lightbox"]', function (event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});
+    $('.slick-testimonial').slick({
+      arrows: false,
+      dots: true,
+    });
+    
+  })(window.jQuery);
